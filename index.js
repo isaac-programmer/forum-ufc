960de2b3da18bf0fa43aad080c167239a87bee17
Isaac Daniel <isaacbusiness2019@hotmail.com>
2024-07-24 00:11:37 -0300

    Inserindo o card de perguntas de exemplo

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+
 const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
 
 app.get("/:nome/:disciplina", (req, res) => {
     var nome = req.params.nome;
