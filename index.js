15ee7becd0989b2ddd7bb60460f3d67e06cb024a
AlanPires01 <alanpires@alu.ufc.br>
2024-07-30 22:07:56 -0300

    juntando navbar e dialogs pt2

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser")
 
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 
@@ -28,6 +32,24 @@ app.get("/:nome/:disciplina", (req, res) => {
         produtos: produtos
     });
 });
+//Rota de receber dados do formulario de login
+app.post("/validarlogin",(req,res)=>{
+    //var user = req.body.usuario;
+    //var senha = req.body.senha;
+})
+
+//Rota de receber dados do formulario de login
+
+//Rota que recebe dados do formulario de cadastro de perguntas
+app.post("/rotaFazerPergunta",(req,res)=>{
+    //var user = req.body.usuario;
+    var senha = req.body.senha;
+})
+
+//Rota que recebe dados do formulario de cadastro de resposta
+app.post("/rotaResposta",(req,res)=>{
+
+})
 
 app.listen(8000, () => {
     console.log("App rodando!");
