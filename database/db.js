22da51ce38ba74b0fc2e07cd324db84d527bcbd9
Davi Rios <manueldavisilveirarios123@gmail.com>
2024-07-30 23:50:50 -0300

    feat: retirada dos logs do banco de dados no console

--- database/db.js
+++ database/db.js
@@ -3,6 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('projeto_tecweb2', 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
+    logging: false,
 });
 
 module.exports = sequelize;
